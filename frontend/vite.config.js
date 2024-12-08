e8aa2a4c5ce420e7f6d0757db393d1e7d095d112
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-08 07:46:01 +0530

    24th

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
-})
+  server: {
+    port: 5173,
+    strictPort: true, // Exit with an error if the port is in use
+    hmr: {
+      overlay: true, // Display errors in the browser
+    },
+  },
+});
+
