827515efcb51701cb7e857e3e3089695a76c301d
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-08 19:50:43 +0530

    .37th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173},
-  resolve: {
-    alias: {
-      // Ensure Vite resolves the react-toastify CSS properly
-      'react-toastify/dist/ReactToastify.css': 'node_modules/react-toastify/dist/ReactToastify.css'
+  build: {
+    rollupOptions: {
+      external: ['react-toastify']
     }
   }
-})
+});
+
