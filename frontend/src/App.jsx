a46c03d5fea76f8c3ed45b29c06958ba093dcd2d
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-08 09:48:39 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,8 @@ import MyOrders from './pages/MyOrders/MyOrders.jsx'
 import SignInSignUp from './components/SignINSignUp/SignInSignUp.jsx'
 import Product from './pages/Product/Product.jsx'
 import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css'; 
+import 'react-toastify/dist/ReactToastify.css';
+
 import Cart from './pages/Cart/Cart.jsx'
 import Checkout from './pages/Checkout/Checkout.jsx'
 import Verify from './pages/Verify/Verify.jsx'
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
-})
+  build: {
+    rollupOptions: {
+      plugins: [
+        {
+          name: 'resolve',
+          resolveId(source) {
+            if (source === 'react-toastify') {
+              return { id: 'react-toastify', external: true };
+            }
+          },
+        },
+      ],
+    },
+  },
+});
+
