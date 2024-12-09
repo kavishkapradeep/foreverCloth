837c8ccb9f011af8ac14e8b22a9346df1bae0966
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-09 10:55:40 +0530

    43th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,17 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173},
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        // Skip specific warnings if needed
-        if (warning.code === 'UNRESOLVED_IMPORT') {
-          console.warn('Skipping unresolved import:', warning.source);
-          return;
-        }
-        warn(warning);
-      },
-    },
-  },
+  server:{port:5173}
 })
