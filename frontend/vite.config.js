d7c5f5fa505568ff9d6d31b5a03b436644377230
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-10 19:21:16 +0530

    85th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server:{port:5174},
-  build: {
-    rollupOptions: {
-      // Example: Handle or ignore specific warnings
-      external: ['react-toastify'],
+  resolve: {
+    alias: {
+      'react-toastify': path.resolve(__dirname, 'node_modules/react-toastify'),
     },
   },
 });
+
 
 
