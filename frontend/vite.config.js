62c135ae9da6ac1c5196b2e469717b8333df8905
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-10 21:00:24 +0530

    100th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-router-dom'], // Externalize react-router-dom
+      external: ['react-router-dom', 'react-toastify'], // Externalize both react-router-dom and react-toastify
     },
   },
 });
+
