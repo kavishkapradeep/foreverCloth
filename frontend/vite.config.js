058e8be8b30c46bc1a02177ea3430ad3741c9b1d
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-10 19:54:23 +0530

    90th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server:[5174],
   resolve: {
     alias: {
-      'react-toastify': '/node_modules/react-toastify',
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-toastify', 'react-toastify/dist/ReactToastify.css'],
+      'react-router-dom': '/node_modules/react-router-dom',
     },
   },
 });
