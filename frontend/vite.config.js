faf6f96d8a79a952e41afd368a6345a7eedbaaa7
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-11 20:09:24 +0530

    124th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
-  plugins:[react()],
-  
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === 'UNRESOLVED_IMPORT') {
-          console.error('Unresolved import:', warning);
-        } else {
-          warn(warning);
-        }
-      },
-    },
-  },
+  plugins: [react()],
 });
