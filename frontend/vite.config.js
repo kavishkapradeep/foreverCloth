62fd0f147c7c0911d2190aba7ff03d06c46c033e
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-11 22:52:55 +0530

    127th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default {
   plugins: [react()],
-});
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'SOME_WARNING_CODE') return; // Ignore specific warnings
+        warn(warning); // Log all other warnings
+      },
+    },
+  },
+};
