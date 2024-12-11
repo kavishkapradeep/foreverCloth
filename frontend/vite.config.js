58cbe34f6cdf4f4639ad3fea0eaddd71d316113f
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-11 11:09:10 +0530

    104 th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import React from 'react';
 
 export default defineConfig({
-  plugins:[React()],
+  plugins:[react()],
   
   build: {
     rollupOptions: {
