9fa311ff657927b109ec326bf6d752d3f84216db
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-11 10:46:54 +0530

    101 th  commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  port:[5174],
   build: {
     rollupOptions: {
       external: ['react-router-dom', 'react-toastify'], // Externalize both react-router-dom and react-toastify
