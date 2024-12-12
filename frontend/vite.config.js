4352d22729ff6210dd462f3444e03ecbc8fe9564
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-12 13:25:37 +0530

    136th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
   build: {
     rollupOptions: {
       onwarn(warning, warn) {
