a8a0890e39a11bc881e420f1313ebb9780c787a4
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-12-13 15:19:08 +0530

    142 th

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,22 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
   build: {
+    outDir: 'dist',
     rollupOptions: {
-      external: ['react-toastify'], // Externalize the module
-    },
-  },// Use './' for relative paths
-});
+      onwarn(warning, warn) {
+        // Log all warnings to the console
+        console.warn('Rollup warning:', warning);
 
+        // Optionally suppress specific warnings
+        if (warning.code === 'UNRESOLVED_IMPORT') {
+          console.warn(`Unresolved import: ${warning.source}`);
+          return;
+        }
+
+        // Pass all other warnings to Rollup
+        warn(warning);
+      },
+    },
+  },
+});
